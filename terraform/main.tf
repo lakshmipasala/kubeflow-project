48bd3edd41176121b1b98017edfa5121dd9f3f51
Lakshmi Pasala <lakshmipasala2@gmail.com>
2026-06-28 00:03:44 +0530

    kubeflow project

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,12 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 }
+
 module "vpc" {
   source = "./modules/vpc"
   availability_zones = var.availability_zones
