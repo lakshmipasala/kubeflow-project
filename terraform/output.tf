05012760f955311cd9cc1fa2bf1aa20204a20547
Lakshmi Pasala <lakshmipasala2@gmail.com>
2026-06-28 11:17:21 +0530

    kubeflow project

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,14 +1,11 @@
 output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
+  value = module.vpc.vpc_id
 }
 
-output "public_subnet_ids" {
-  description = "List of public subnet IDs"
-  value       = aws_subnet.public.id
+output "public_subnet_id" {
+  value = module.vpc.public_subnet_id
 }
 
-output "private_subnet_ids" {
-  description = "List of private subnet IDs"
-  value       = aws_subnet.private.id
+output "private_subnet_id" {
+  value = module.vpc.private_subnet_id
 }
