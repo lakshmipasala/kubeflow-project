52c4b8225c0c0600d6dfbee85941c0084101116c
Lakshmi Pasala <lakshmipasala2@gmail.com>
2026-06-28 11:28:57 +0530

    kubeflow project

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -89,7 +89,7 @@ resource "aws_eks_node_group" "nodes" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "managed-nodes"
   node_role_arn   = aws_iam_role.nodes.arn
-  subnet_ids     = aws_subnet.private.id
+  subnet_ids     = [var.private_subnet_id]
 
   scaling_config {
     desired_size = 2
